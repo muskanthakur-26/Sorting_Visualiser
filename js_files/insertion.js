6c491d26677d80c0f0a3fbc2737d1912447d5f68
Mohit Singh <76548971+Terminator1199@users.noreply.github.com>
2022-01-20 22:47:41 +0530

    Add files via upload

---
+++ js_files/insertion.js
@@ -0,0 +1,34 @@
+async function insertion(){
+  const ele = document.querySelectorAll(".bar");
+  ele[0].style.background = '#d9534f';
+  for(let i = 1; i < ele.length; i++){
+      let j = i - 1;
+      let key = ele[i].style.height;
+      ele[i].style.background = '#f0ad4e';
+      await waitforme(260);
+      while(j >= 0 && (parseInt(ele[j].style.height) > parseInt(key))){
+          ele[j].style.background = '#f0ad4e';
+          ele[j + 1].style.height = ele[j].style.height;
+          j--;
+          await waitforme(260);
+          for(let k = i; k >= 0; k--){
+              ele[k].style.background = '#d9534f';
+          }
+      }
+      ele[j + 1].style.height = key;
+      ele[i].style.background ='#d9534f';
+  }
+  for(let i = 0; i < ele.length; i++){
+    ele[i].style.background = '	#00FA9A';
+  }
+}
+const inSortbtn = document.querySelector(".insertionSort");
+inSortbtn.addEventListener('click', async function(){
+  disableSortingBtn();
+  disableNewArrayBtn();
+  await insertion();
+  enableSortingBtn();
+  enableNewArrayBtn();
+});
+
+
---
+++ js_files/selection.js
@@ -0,0 +1,32 @@
+async function selection(){
+  const ele = document.querySelectorAll(".bar");
+  for(let i = 0; i < ele.length; i++){
+      let min_index = i;
+      ele[i].style.background = '#d9534f';
+      for(let j = i+1; j < ele.length; j++){
+          ele[j].style.background = '#f0ad4e';
+          await waitforme(260);
+          if(parseInt(ele[j].style.height) < parseInt(ele[min_index].style.height)){
+              if(min_index !== i){
+                  ele[min_index].style.background = 'white';
+              }
+              min_index = j;
+          } 
+          else{
+              ele[j].style.background = 'white';
+          }   
+      }
+      await waitforme(260);
+      swap(ele[min_index], ele[i]);
+      ele[min_index].style.background = 'white';
+      ele[i].style.background = '#00FA9A';
+  }
+}
+const selectionSortbtn = document.querySelector(".selectionSort");
+selectionSortbtn.addEventListener('click', async function(){
+  disableSortingBtn();
+  disableNewArrayBtn();
+  await selection();
+  enableSortingBtn();
+  enableNewArrayBtn();
+});
