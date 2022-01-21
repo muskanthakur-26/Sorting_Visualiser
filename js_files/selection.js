865fdab28ca682b618da67836be3fbf615ffe4b8
Muskan <76208395+muskanthakur-26@users.noreply.github.com>
2022-01-21 12:07:50 +0530

    Update selection.js

--- js_files/selection.js
+++ js_files/selection.js
@@ -5,7 +5,7 @@ async function selection(){
       ele[i].style.background = '#d9534f';
       for(let j = i+1; j < ele.length; j++){
           ele[j].style.background = '#f0ad4e';
-          await waitforme(260);
+          await waitforme(150);
           if(parseInt(ele[j].style.height) < parseInt(ele[min_index].style.height)){
               if(min_index !== i){
                   ele[min_index].style.background = 'white';
@@ -16,7 +16,7 @@ async function selection(){
               ele[j].style.background = 'white';
           }   
       }
-      await waitforme(260);
+      await waitforme(150);
       swap(ele[min_index], ele[i]);
       ele[min_index].style.background = 'white';
       ele[i].style.background = '#00FA9A';
